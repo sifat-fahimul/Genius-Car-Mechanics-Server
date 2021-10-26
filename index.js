21b0ee5dc488dad362e45c1ac58947787f2f430e
sifat-fahimul <mdsifatfeni@gmail.com>
2021-10-27 02:05:06 +0600

    update db pass

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function run() {
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-    res.send('running genius server')
+    res.send('running  server')
 });
 
 app.listen(port, () => {
